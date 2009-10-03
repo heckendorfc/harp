c7984f61a22ad0d29f7e62b84171f6196846e0ce
Christian Heckendorf <heckendorfc@gmail.com>
2009-10-03 16:44:52 +0000

    Adding good harp jackdrc values to osx_install.sh

--- osx_install.sh
+++ osx_install.sh
@@ -4,3 +4,4 @@ autoreconf -if
 ./configure --with-jack CFLAGS="-I/opt/local/include" LDFLAGS="-L/opt/local/lib -L/opt/local/lib/jack"
 make
 sudo make install
+echo '/opt/local/bin/jackd -dcoreaudio -p1024 -r44100 -P' >> ~/.jackdrc
