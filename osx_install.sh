3bb28b1fbf557bba684f6bd19e7c510a9e54bd6c
Christian Heckendorf <heckendorfc@gmail.com>
2012-10-29 17:38:09 +0000

    Adding extra information for OSX users.

--- osx_install.sh
+++ osx_install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-glibtoolize --copy --force &&
-autoreconf -if
+#glibtoolize --copy --force && autoreconf -if
 ./configure --with-jack CFLAGS="-I/opt/local/include" LDFLAGS="-L/opt/local/lib -L/opt/local/lib/jack"
 make
 sudo make install
