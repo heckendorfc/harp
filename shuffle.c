c6659efdae492f1efa4a677353f2294a3896da68
Christian Heckendorf <heckendorfc@gmail.com>
2012-11-04 20:00:25 +0000

    Rewriting core shuffle function.

--- shuffle.c
+++ shuffle.c
@@ -20,83 +20,12 @@
 #include "dbact.h"
 #include "util.h"
 
-const unsigned int primes[] = {
-	2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79,
-	83, 89, 97, 103, 109, 113, 127, 137, 139, 149, 157, 167, 179, 193, 199, 211, 227, 241, 257,
-	277, 293, 313, 337, 359, 383, 409, 439, 467, 503, 541, 577, 619, 661, 709, 761, 823, 887,
-	953, 1031, 1109, 1193, 1289, 1381, 1493, 1613, 1741, 1879, 2029, 2179, 2357, 2549, 2753, 2971,
-	3209, 3469, 3739, 4027, 4349, 4703, 5087, 5503, 5953, 6427, 6949, 7517, 8123, 8783, 9497, 10273,
-	11113, 12011, 12983, 14033, 15173, 16411, 17749, 19183, 20753, 22447, 24281, 26267, 28411, 30727,
-	33223, 35933, 38873, 42043, 45481, 49201, 53201, 57557, 62233, 67307, 72817, 78779, 85229, 92203,
-	99733, 107897, 116731, 126271, 136607, 147793, 159871, 172933, 187091, 202409, 218971, 236897,
-	256279, 277261, 299951, 324503, 351061, 379787, 410857, 444487, 480881, 520241, 562841, 608903,
-	658753, 712697, 771049, 834181, 902483, 976369, 1056323, 1142821, 1236397, 1337629, 1447153, 1565659,
-	1693859, 1832561, 1982627, 2144977, 2320627, 2510653, 2716249, 2938679, 3179303, 3439651, 3721303, 4026031,
-	4355707, 4712381, 5098259, 5515729, 5967347, 6456007, 6984629, 7556579, 8175383, 8844859, 9569143, 10352717,
-	11200489, 12117689, 13109983, 14183539, 15345007, 16601593, 17961079, 19431899, 21023161, 22744717,
-	24607243, 26622317, 28802401, 31160981, 33712729, 36473443, 39460231, 42691603, 46187573, 49969847,
-	54061849, 58488943, 63278561, 68460391, 74066549, 80131819, 86693767, 93793069, 101473717, 109783337,
-	118773397, 128499677, 139022417, 150406843, 162723577, 176048909, 190465427, 206062531, 222936881, 241193053,
-	260944219, 282312799, 305431229, 330442829, 357502601, 386778277, 418451333, 452718089, 489790921, 529899637,
-	573292817, 620239453, 671030513, 725980837, 785430967, 849749479, 919334987, 994618837, 1076067617, 1164186217,
-	1259520799u, 1362662261u, 1474249943u, 1594975441u, 1725587117u, 1866894511u, 2019773507u, 2185171673u,
-	2364114217u, 2557710269u, 2767159799u, 2993761039u, 3238918481u, 3504151727u, 3791104843u, 4101556399u, 4294967291u
-};
-
-
-struct prime_rand_data{
-	unsigned int items;
-	unsigned int prime;
-	unsigned int searches;
-	unsigned int current;
-	unsigned int skip;
-	unsigned int count;
+struct shuffle_data{
+	int count;
+	int batch_count;
+	int tail;
 	char *query;
 };
-
-static int prime_rand_next(struct prime_rand_data *data){
-	if(data->searches==data->prime)
-		return -1;
-	int next=data->current;
-	while(1){
-		next+=data->skip;
-		next%=data->prime;
-		data->searches++;
-		if(next<data->items){
-			data->current=next;
-			break;
-		}
-	}
-	return data->current;
-}
-
-static struct prime_rand_data *prime_rand_init(int items){
-	if(!items)return NULL;
-	srandom((unsigned int)time(NULL));
-
-	const unsigned int *nextprime=primes;
-	while(*nextprime<items)
-		nextprime++;
-
-	struct prime_rand_data *data=malloc(sizeof(*data));
-	if(!data){
-		debug(2,"Shuffle struct malloc failed.");
-		return NULL;
-	}
-	data->items=items;
-	data->prime=*nextprime;
-	data->count=data->searches=0;
-
-	unsigned int a = (random()%13)+1;
-	unsigned int b = (random()%7)+1;
-	unsigned int c = (random()%5)+1;
-	data->current = (random()%items);
-	data->skip = (a*items*items)+(b*items)+c;
-	data->skip &= ~0xC0000000;
-	if(data->skip%data->prime==0)data->skip++;
-
-	return data;
-}
 
 static struct shuffle_queries{
 	char group;
@@ -107,7 +36,8 @@ static struct shuffle_queries{
 	{	's',
 		"SELECT COUNT(SelectID) FROM TempSelect WHERE TempID=%d",
 		"SELECT SelectID FROM TempSelect WHERE TempID=%d",
-		"UPDATE TempPlaylistSong SET \"Order\"=\"Order\"*-1"
+		"SELECT SongID,\"Order\",%d FROM TempPlaylistSong WHERE \"Order\"<0 ORDER BY \"Order\" ASC"
+		//"UPDATE TempPlaylistSong SET \"Order\"=\"Order\"*-1"
 	},
 	{	'a',
 		"SELECT COUNT(SongID) FROM Song WHERE SongID IN (SELECT SelectID FROM TempSelect,Song WHERE SelectID=SongID AND TempID=%d GROUP BY AlbumID)",
@@ -130,18 +60,36 @@ static void fillTempPlaylistSong(int tempid,int group){
 	harp_sqlite3_exec(conn,query,batch_tempplaylistsong_insert_cb,&data,NULL);
 }
 
+static int rand_next(struct shuffle_data *data){
+	return random()%data->count;
+}
+
+static int clean_shuffle_cb(void *arg, int col_count, char **row, char **titles){
+	unsigned int order;
+	struct shuffle_data *data=(struct shuffle_data*)arg;
+	sprintf(data->query,"UPDATE TempPlaylistSong SET \"Order\"=%d WHERE SongID=%s",-(data->tail++),*row);
+	harp_sqlite3_exec(conn,data->query,NULL,NULL,NULL);
+	data->batch_count++;
+	if(data->count>DB_BATCH_SIZE){
+		data->batch_count=0;
+		harp_sqlite3_exec(conn,"COMMIT",NULL,NULL,NULL);
+		harp_sqlite3_exec(conn,"BEGIN",NULL,NULL,NULL);
+	}
+	return 0;
+}
+
 static int shuffle_cb(void *arg, int col_count, char **row, char **titles){
 	unsigned int order;
-	struct prime_rand_data *data=(struct prime_rand_data*)arg;
-	if((order=prime_rand_next(data))==-1){
-		debug(2,"prime_rand_next returned -1");
+	struct shuffle_data *data=(struct shuffle_data*)arg;
+	if((order=rand_next(data))==-1){
+		debug(2,"rand_next returned -1");
 		return 1;
 	}
 	sprintf(data->query,"INSERT INTO TempPlaylistSong(SongID,\"Order\") VALUES(%s,%d)",*row,-(order+1));
 	harp_sqlite3_exec(conn,data->query,NULL,NULL,NULL);
-	data->count++;
+	data->batch_count++;
 	if(data->count>DB_BATCH_SIZE){
-		data->count=0;
+		data->batch_count=0;
 		harp_sqlite3_exec(conn,"COMMIT",NULL,NULL,NULL);
 		harp_sqlite3_exec(conn,"BEGIN",NULL,NULL,NULL);
 	}
@@ -152,7 +100,10 @@ void shuffle(int list){
 	char query[350],cb_query[350];
 	int tempid,group=0;
 	unsigned int items=0;
+	struct shuffle_data data;
 
+	srandom((unsigned int)time(NULL));
+	
 	if(arglist[ASHUFFLE].active && arglist[ASHUFFLE].subarg){
 		for(group=0;shuffle_q[group].group && shuffle_q[group].group!=*arglist[ASHUFFLE].subarg;group++);
 		if(!shuffle_q[group].group)
@@ -167,30 +118,31 @@ void shuffle(int list){
 	else{
 		tempid=insertTempSelectQuery("SELECT %d,SongID FROM TempPlaylistSong");
 	}
+	
 	sprintf(query,shuffle_q[group].count,tempid);
 	harp_sqlite3_exec(conn,query,uint_return_cb,&items,NULL);
 	sprintf(query,shuffle_q[group].primary_select,tempid);
 	
 	harp_sqlite3_exec(conn,"DELETE FROM TempPlaylistSong",NULL,NULL,NULL);
 
-	struct prime_rand_data *data=prime_rand_init(items);
-	if(!data)
-		return;
-	data->query=cb_query;
-
+	data.count=items;
+	data.batch_count=0;
+	data.query=cb_query;
 	harp_sqlite3_exec(conn,"BEGIN",NULL,NULL,NULL);
-	harp_sqlite3_exec(conn,query,shuffle_cb,data,NULL);
+	harp_sqlite3_exec(conn,query,shuffle_cb,&data,NULL);
 	harp_sqlite3_exec(conn,"COMMIT",NULL,NULL,NULL);
 
-	if(group>0)
-		fillTempPlaylistSong(tempid,group);
-	else{
-		sprintf(query,shuffle_q[group].fill,tempid);
-		harp_sqlite3_exec(conn,query,NULL,NULL,NULL);
-	}
+	data.count=items;
+	data.batch_count=0;
+	data.tail=items+1;
+	sprintf(query,"SELECT b.SongID,b.SongID FROM TempPlaylistSong a, TempPlaylistSong b WHERE a.SongID<b.SongID AND a.\"Order\"=b.\"Order\" GROUP BY b.SongID");
+	harp_sqlite3_exec(conn,"BEGIN",NULL,NULL,NULL);
+	harp_sqlite3_exec(conn,query,clean_shuffle_cb,&data,NULL);
+	harp_sqlite3_exec(conn,"COMMIT",NULL,NULL,NULL);
+
+	fillTempPlaylistSong(tempid,group);
 
 	harp_sqlite3_exec(conn,"DELETE FROM TempPlaylistSong WHERE \"Order\"<1",NULL,NULL,NULL); /* Just in case */
-	free(data);
 	if(arglist[AZSHUFFLE].active){
 		debug(1,"Z Shuffling");
 		zshuffle(items);
